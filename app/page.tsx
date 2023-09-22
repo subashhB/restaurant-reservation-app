04bd13febcf4a9a5b13e22198d3b078ffb8aa39c
subashhB <subashbaidya9@gmail.com>
2023-09-22 12:32:05 +0545

    Restaurant Description Complete

--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,71 @@ export default function Home() {
                 quisquam illum alias?
               </p>
             </div>
+            {/* Image Gallery */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                5 photos
+              </h1>
+              <div className="flex flex-wrap">
+                <img
+                  className="w-56 h-47 mr-1 mb-1"
+                  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+                  alt="Image"
+                />
+                <img
+                  className="w-56 h-47 mr-1 mb-1"
+                  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+                  alt="Image"
+                />
+                <img
+                  className="w-56 h-47 mr-1 mb-1"
+                  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+                  alt="Image"
+                />
+                <img
+                  className="w-56 h-47 mr-1 mb-1"
+                  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+                  alt="Image"
+                />
+                <img
+                  className="w-56 h-47 mr-1 mb-1"
+                  src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
+                  alt="Image"
+                />
+              </div>
+            </div>
+            {/* Reviews */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                What 100 People are saying
+              </h1>
+              <div>
+                {/* Review Card */}
+                <div className="border-b p-7 mb-7">
+                  <div className="flex">
+                    <div className="w-1/6 flex flex-col items-center">
+                      <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                        <h2 className="text-white text-2xl">SB</h2>
+                      </div>
+                      <p className="text-center">Subash Baidya</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="flex mr-5">*****</div>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-light text-lg">
+                          Lorem ipsum dolor sit amet consectetur, adipisicing
+                          elit. Commodi, aliquid blanditiis repellendus facere
+                          dolorem, doloribus nostrum, reprehenderit veritatis
+                          odit mollitia vel provident recusandae deserunt.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </main>
