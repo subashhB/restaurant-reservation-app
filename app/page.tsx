91842db84419faaaa2b60b01c9616553065d1e9e
subashhB <subashbaidya9@gmail.com>
2023-09-28 10:28:42 +0545

    Defining the Interface for Restuarant Cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,41 @@
-import { PrismaClient } from "@prisma/client";
+import { Cuisine, Location, PRICE, PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 
 const prisma = new PrismaClient();
 
-const fetchRestaurants = async () => {
-  return prisma.restaurant.findMany();
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+  return restaurants;
 };
 
 export default async function Home() {
   const restaurants = await fetchRestaurants();
-  console.log(restaurants);
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap">
-        <RestaurantCard />
+        {restaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
       </div>
     </main>
   );
