f05d6c8b7ff5cd2dc4584ad774de66f03829605a
subashhB <subashbaidya9@gmail.com>
2023-09-28 10:58:53 +0545

    Fetching a Restaurant by slug

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,5 @@
+import { PrismaClient } from "@prisma/client";
 import Description from "./components/Description";
-import Header from "./components/Header";
 import ImageGallery from "./components/ImageGallery";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
@@ -7,7 +7,38 @@ import RestaurantNavbar from "./components/RestaurantNavbar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 
-const RestaurantDetails = () => {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchResturantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restuarant = await prisma.restaurant.findUnique({
+    where: {
+      slug: slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    },
+  });
+  if (!restuarant) {
+    throw new Error("Restaurant is not defined.");
+  }
+  return restuarant;
+};
+
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchResturantBySlug(params.slug);
+  console.log(restaurant);
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow ">
