25fd18bffca00a295ded6647c9540bc85eb66343
subashhB <subashbaidya9@gmail.com>
2023-10-03 16:47:18 +0545

    Adding Middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -10,19 +10,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const bearerToken = req.headers["authorization"] as string;
-  if (!bearerToken) {
-    res.status(401).json({ errorMessage: "Unauthorized Request" });
-  }
   const token = bearerToken.split(" ")[1];
-  if (!token) {
-    res.status(401).json({ errorMessage: "Unauthorized Request" });
-  }
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    res.status(401).json({ errorMessage: "Unauthorized Request" });
-  }
   const payload = jwt.decode(token) as { email: string };
   const user = await prisma.user.findUnique({
     where: {
