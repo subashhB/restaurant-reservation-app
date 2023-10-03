8abbe79d9479f9dd6c8d4c8fa741419e60ca64e7
subashhB <subashbaidya9@gmail.com>
2023-10-03 09:44:35 +0545

    Validating User's Input

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,7 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    res.status(200).json({ message: "hello world" });
+    const { firstName, lastName, email, phone, city, password } = req.body;
+    const errors: string[] = [];
+    const validationSchema = [
+      {
+        valid: validator.isLength(firstName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "First Name is Invalid",
+      },
+      {
+        valid: validator.isLength(lastName, {
+          min: 1,
+          max: 20,
+        }),
+        errorMessage: "Last Name is Invalid",
+      },
+      {
+        valid: validator.isEmail(email),
+        errorMessage: "Email is Invalid",
+      },
+      {
+        valid: validator.isMobilePhone(phone),
+        errorMessage: "Phone Number is invalid",
+      },
+      {
+        valid: validator.isLength(city, { min: 1 }),
+        errorMessage: "City is Invalid",
+      },
+      {
+        valid: validator.isStrongPassword(password),
+        errorMessage: "Password is not strong enough!",
+      },
+    ];
+    validationSchema.forEach((check) => {
+      if (!check.valid) {
+        errors.push(check.errorMessage);
+      }
+    });
+    if (errors.length) {
+      res.status(400).json({ errorMessage: errors[0] });
+    }
+    return res.status(200).json({ message: "hello world" });
   }
 }
