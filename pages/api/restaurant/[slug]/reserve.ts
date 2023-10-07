f427b69e27c55c1db73e0ff34bbdd97e62dee579
subashhB <subashbaidya9@gmail.com>
2023-10-07 13:16:42 +0545

    Determining the Tables to Book

--- pages/api/restaurant/[slug]/reserve.ts
+++ pages/api/restaurant/[slug]/reserve.ts
@@ -79,7 +79,34 @@ export default async function handler(
     }
   });
 
+  const tablesToBook: number[] = [];
+  let seatsRemaining = parseInt(partySize);
+
+  while (seatsRemaining > 0) {
+    if (seatsRemaining >= 3) {
+      if (tablesCount[4].length) {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      } else {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      }
+    } else {
+      if (tablesCount[2].length) {
+        tablesToBook.push(tablesCount[2][0]);
+        tablesCount[2].shift();
+        seatsRemaining = seatsRemaining - 2;
+      } else {
+        tablesToBook.push(tablesCount[4][0]);
+        tablesCount[4].shift();
+        seatsRemaining = seatsRemaining - 4;
+      }
+    }
+  }
+
   return res.json({
-    tablesCount,
+    tablesToBooks: tablesToBook,
   });
 }
