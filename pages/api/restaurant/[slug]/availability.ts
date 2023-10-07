6870c72eae2cafe7a4ef15df9f572623d8d09932
subashhB <subashbaidya9@gmail.com>
2023-10-07 09:38:47 +0545

    Fetching the Restaurant Tables

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -55,5 +55,23 @@ export default async function handler(
         };
       }, {});
   });
-  return res.json({ searchTimes, bookings, bookingsTableObj });
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      tables: true,
+    },
+  });
+
+  if (!restaurant) {
+    return res.status(400).json({
+      errorMessage: "Invalid Data Provided.",
+    });
+  }
+
+  const tables = restaurant.tables;
+
+  return res.json({ searchTimes, bookings, bookingsTableObj, tables });
 }
