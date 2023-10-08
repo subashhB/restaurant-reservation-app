cbb07b05d824761102dc62076aea2efad7a6d048
subashhB <subashbaidya9@gmail.com>
2023-10-08 19:05:47 +0545

    Dynamically Rendering Data for the Reservation Page

--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,15 +1,16 @@
-const Header = () => {
+interface HeaderProps {
+  image: string;
+  name: string;
+}
+
+const Header = ({ image, name }: HeaderProps) => {
   return (
     <div>
       <h3 className="font-bold">You&apos;re almost done!</h3>
       <div className="mt-5 flex">
-        <img
-          className="w-32 h-18 rounded"
-          src="https://img.freepik.com/free-photo/top-view-table-full-delicious-food-composition_23-2149141353.jpg"
-          alt=""
-        />
+        <img className="w-32 h-18 rounded" src={image} alt="" />
         <div className="ml-4">
-          <h1 className="font-bold text-3xl">Restaurant Name</h1>
+          <h1 className="font-bold text-3xl">{name}</h1>
           <div className="flex mt-3">
             <p className="mr-6">Tue, 22, 2023</p>
             <p className="mr-6">7:30 pm</p>
--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,11 +1,30 @@
+import { PrismaClient } from "@prisma/client";
 import Form from "./components/Form";
 import Header from "./components/Header";
+import { notFound } from "next/navigation";
 
-const Reservation = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
+
+const Reservation = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
   return (
     <div className="border-t h-screen ">
       <div className="py-9 w-3/5 m-auto">
-        <Header />
+        <Header image={restaurant.main_image} name={restaurant.name} />
         <Form />
       </div>
     </div>
