672fbe640a71d67d8a206a15bd6ea14b122e5d01
subashhB <subashbaidya9@gmail.com>
2023-10-08 18:54:33 +0545

    Creating the Bookings and Linking it to the Tables

--- pages/api/restaurant/[slug]/availability.ts
+++ pages/api/restaurant/[slug]/availability.ts
@@ -10,66 +10,68 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { slug, day, time, partySize } = req.query as {
-    slug: string;
-    day: string;
-    time: string;
-    partySize: string;
-  };
-  const restaurant = await prisma.restaurant.findUnique({
-    where: {
-      slug,
-    },
-    select: {
-      tables: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
-
-  if (!restaurant) {
-    return res.status(400).json({
-      errorMessage: "Invalid Data Provided.",
+  if (req.method === "GET") {
+    const { slug, day, time, partySize } = req.query as {
+      slug: string;
+      day: string;
+      time: string;
+      partySize: string;
+    };
+    const restaurant = await prisma.restaurant.findUnique({
+      where: {
+        slug,
+      },
+      select: {
+        tables: true,
+        open_time: true,
+        close_time: true,
+      },
     });
+
+    if (!restaurant) {
+      return res.status(400).json({
+        errorMessage: "Invalid Data Provided.",
+      });
+    }
+
+    const searchTimesWithTables = await findAvailableTables({
+      day,
+      time,
+      res,
+      restaurant,
+    });
+
+    if (!searchTimesWithTables)
+      return res.status(400).json({ errorMessage: "Invalid Data Provided" });
+
+    if (!day || !time || !partySize) {
+      return res.status(400).json({
+        errorMessage: "Invalid Data Provided.",
+      });
+    }
+
+    const availability = searchTimesWithTables
+      .map((t) => {
+        const sumSeats = t.tables.reduce((sum, table) => {
+          return sum + table.seats;
+        }, 0);
+
+        return {
+          time: t.time,
+          available: sumSeats >= parseInt(partySize),
+        };
+      })
+      .filter((availability) => {
+        const timeIsAfterOpeningHour =
+          new Date(`${day}T${availability.time}`) >=
+          new Date(`${day}T${restaurant.open_time}`);
+        const timeIsAfterClosingHour =
+          new Date(`${day}T${availability.time}`) <=
+          new Date(`${day}T${restaurant.close_time}`);
+
+        return timeIsAfterClosingHour && timeIsAfterOpeningHour;
+      });
+
+    return res.json(availability);
   }
-
-  const searchTimesWithTables = await findAvailableTables({
-    day,
-    time,
-    res,
-    restaurant,
-  });
-
-  if (!searchTimesWithTables)
-    return res.status(400).json({ errorMessage: "Invalid Data Provided" });
-
-  if (!day || !time || !partySize) {
-    return res.status(400).json({
-      errorMessage: "Invalid Data Provided.",
-    });
-  }
-
-  const availability = searchTimesWithTables
-    .map((t) => {
-      const sumSeats = t.tables.reduce((sum, table) => {
-        return sum + table.seats;
-      }, 0);
-
-      return {
-        time: t.time,
-        available: sumSeats >= parseInt(partySize),
-      };
-    })
-    .filter((availability) => {
-      const timeIsAfterOpeningHour =
-        new Date(`${day}T${availability.time}`) >=
-        new Date(`${day}T${restaurant.open_time}`);
-      const timeIsAfterClosingHour =
-        new Date(`${day}T${availability.time}`) <=
-        new Date(`${day}T${restaurant.close_time}`);
-
-      return timeIsAfterClosingHour && timeIsAfterOpeningHour;
-    });
-
-  return res.json(availability);
 }
